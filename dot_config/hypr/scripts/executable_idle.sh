3d559d313f264c3c4cdeef181ca2958b11d6fec8
Steve Loveless <steve.loveless@gmail.com>
2023-09-06 23:22:27 -0700

    Add swaync, bind for swaylock, other minor things

--- dot_config/hypr/scripts/executable_idle.sh
+++ dot_config/hypr/scripts/executable_idle.sh
@@ -1,6 +1,6 @@
 swayidle -w \
-  timeout 30 'swaylock --daemonize' \
-  timeout 36 'hyprctl dispatch dpms off' \
+  timeout 300 'swaylock --daemonize' \
+  timeout 360 'hyprctl dispatch dpms off' \
   resume 'hyprctl dispatch dpms on && kanshi' \
   before-sleep 'swaylock --daemonize' \
   lock 'swaylock --daemonize'
---
+++ dot_config/swaync/NOTES.md
@@ -0,0 +1,2 @@
+Pulled from https://github.com/lvntcnylmz/dotfiles/blob/main/.config/swaync/style.css
+...which came from https://github.com/ErikReider/SwayNotificationCenter/discussions/183.
